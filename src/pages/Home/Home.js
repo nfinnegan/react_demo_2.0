869d1949a6897a53d5c406b53bcd159a2d0b385a
nfinnegan <nfinneg2@gmail.com>
2022-08-11 10:15:33 -0500

    wrkin on adding entrace imgs

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -3,5 +3,36 @@ import "./style.css";
 import { Link } from "react-router-dom";
 
 export const Home = () => {
-  return <div className="homeWrapper">Testing</div>;
+  return (
+    <div className="homeWrapper">
+      <div className="container toggleImages">
+        <div>
+          <img
+            id="img1"
+            src="/assets/toggle.png"
+            className="img-responsive"
+            alt="toggleThumbsUp"
+          />
+          <div>
+            <br />
+            <br />
+            <br />
+
+            <button>Enter Here</button>
+          </div>
+        </div>
+        <div>
+          <img
+            id="img2"
+            className="img-responsive"
+            src="/assets/LD-Toggle-Posed-Lounge-v11Aalpha.png"
+            alt='toggleLaidBack"'
+          />
+          <div>
+            <button>Enter Here</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
